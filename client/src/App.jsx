e42742f5c85c0cda3de1e971a68091680221659d
Shruti-Mishra27 <shrutimishra.2743@gmail.com>
2023-10-12 21:26:49 +0530

    VideoUpload Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import DataSet from './components/DataSet/DataSet';
 import DetectedVideo from './components/DetectedVideo/Detect';
 import Home from "./components/Home/Home";
+import Video_edit from './components/VideoUpload_edit/Video_edit';
 
 function App() {
 
   return (
     <>
-    <DataSet />
+    <Video_edit />
     </>
   )
 }
