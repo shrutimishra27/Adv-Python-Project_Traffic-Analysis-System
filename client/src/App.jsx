caa74f4c7375d9b83bf99b6e41edddd179c7ca4c
Shruti-Mishra27 <shrutimishra.2743@gmail.com>
2023-10-13 10:29:01 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import './App.css'
-import DataSet from './components/DataSet/DataSet';
-import DetectedVideo from './components/DetectedVideo/Detect';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import DataSet from "./components/DataSet/DataSet";
+import DetectedVideo from "./components/DetectedVideo/Detect";
 import Home from "./components/Home/Home";
-import Video from './components/VideoUpload/Video';
+import Video from "./components/VideoUpload/Video";
 
 function App() {
-
   return (
     <>
-    <Video />
+     <Video />
+     <DataSet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/VideoUpload/Video.jsx
+++ client/src/components/VideoUpload/Video.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./Video.css";
-import video from "./detected_video.mp4"
+import video from "./detected_video.mp4";
 
 const Video = () => {
   const [uploading, setUploading] = useState(false);
@@ -18,6 +18,27 @@ const Video = () => {
     setShowPopup(false);
   };
 
+  const downloadVideo = () => {
+    // Replace 'video-url.mp4' with the actual URL or path of your MP4 video
+    const videoUrl = "video-url.mp4";
+
+    fetch(videoUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "video.mp4"; // You can change the filename here
+        a.style.display = "none";
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch((error) => {
+        console.error("Error downloading the video:", error);
+      });
+  };
+
   useEffect(() => {
     if (showPopup) {
       // You can add your code here to display the second video in a popup
@@ -26,39 +47,47 @@ const Video = () => {
 
   return (
     <>
-    
-    <h1 style={{ textTransform: 'uppercase', marginTop:'10px'}}>Get a Detected Video </h1>
-    <div className="vcontainer">
-      {uploading ? (
-        <p className="uploading-message">Uploading and Detecting Video...</p>
-      ) : (
-        <div className="video-uploader">
-          <input
-            type="file"
-            accept="video/*"
-            onChange={handleUpload}
-            id="fileInput"
-          />
-          <h2>Upload a Video</h2>
-          <h3>And get Detected Video</h3>
-          <br />
-          <img className="v-image" src="https://copyassignment.com/wp-content/uploads/2022/06/Violation_Detection_Frame-1024x602.jpg" alt="" />
-          <br />
-          <br />
-          <br />
-          <label htmlFor="fileInput">Upload Video</label>
-        </div>
-      )}
-      {showPopup && (
-        <div className="popup">
-          <video controls>
-            <source src={video} type="video/mp4" className="detected" />
-            Your browser does not support the video tag.
-          </video>
-          <button onClick={closePopup}>Close</button>
-        </div>
-      )}
-    </div>
+      <h1 style={{ textTransform: "uppercase", marginTop: "10px" }}>
+        Get a Detected Video{" "}
+      </h1>
+      <div className="vcontainer">
+        {uploading ? (
+          <p className="uploading-message">Uploading and Detecting Video...</p>
+        ) : (
+          <div className="video-uploader">
+            <input
+              type="file"
+              accept="video/*"
+              onChange={handleUpload}
+              id="fileInput"
+            />
+            <h2>Upload a Video</h2>
+            <h3>And get Detected Video</h3>
+            <br />
+            <img
+              className="v-image"
+              src="https://copyassignment.com/wp-content/uploads/2022/06/Violation_Detection_Frame-1024x602.jpg"
+              alt=""
+            />
+            <br />
+            <br />
+            <br />
+            <label htmlFor="fileInput">Upload Video</label>
+          </div>
+        )}
+        {showPopup && (
+          <div className="popup">
+            <video controls>
+              <source src={video} type="video/mp4" className="detected" />
+              Your browser does not support the video tag.
+            </video>
+            <div className="button-container">
+              <button onClick={closePopup}>Close</button>
+              <button onClick={downloadVideo}>Download</button>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
