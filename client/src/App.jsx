c541ac64d60356e38f72d1d5a4b8fee0854ce9df
shivraj <nakumshivraj3@gmail.com>
2023-11-03 09:03:38 +0530

    #

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Home from "./components/Home/Home";
+import Blog from "./components/Blog/Blog";
 
 function App() {
 
   return (
-   <Home />
+    <>
+    {/* <Home /> */}
+    <Blog />
+    </>
+
   )
 }
 
